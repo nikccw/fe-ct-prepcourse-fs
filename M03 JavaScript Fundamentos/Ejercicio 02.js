03c558162f9b26a841069eacfb9c09f560a5bed1
nikccw <bik876.vaca@gmail.com>
2023-03-08 00:20:00 -0600

    prueba

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,11 +12,13 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x+ y
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y
 }
 
 function divide(x, y) {
